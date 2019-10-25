fa5d636463dfd9d7fc00eedf6e6f3364af8060dc
Marcus Dillavou <dillavou@vipaar.com>
2019-10-25 15:03:11 -0500

    Downgrade oauth2 to be compatible with the other backends

--- mix.exs
+++ mix.exs
@@ -41,7 +41,7 @@ defmodule UeberauthAuth0.Mixfile do
   defp deps do
     [
       {:ueberauth, "~> 0.6"},
-      {:oauth2, "~> 2.0"},
+      {:oauth2, "~> 0.9"},
 
       # Docs:
       {:ex_doc, "~> 0.21", only: :dev},
